528e974451451529a31d59f934638383f4e1678a
5kydriven <tagpuno619@gmail.com>
2024-08-03 09:24:22 +0800

    article design done

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <nav class="w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-    <div class="px-3 py-3 lg:px-5 lg:pl-3">
+  <nav class="w-full bg-white border-b sticky top-0 z-50  border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <div class="px-3 py-3 lg:px-5 lg:pl-10">
       <div class="flex items-center justify-between">
         <a href="https://flowbite.com" class="flex items-center">
           <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -29,8 +29,8 @@ onBeforeMount(() => {
     </div>
   </nav>
 
-  <div class="flex relative h-[89vh]">
-    <aside class="w-80 px-3 py-4 h-full border-0 border-r">
+  <div class="flex relative">
+    <aside class="w-[20rem] px-3 py-4 ps-11 h-full flex-none">
       <form class="max-w-sm mx-auto">
         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
         <textarea id="message" rows="4"
@@ -38,10 +38,29 @@ onBeforeMount(() => {
           placeholder="Leave a comment..."></textarea>
       </form>
     </aside>
-    <main>
-      <div>
-        hello
-      </div>
+    <main class="w-full h-full border-0 border-l flex-1">
+      <article class="flex gap-2 p-2 w-[35rem]" v-for="n in 10">
+        <div class="pt-2">
+          <img class="w-10 h-10 rounded-full" src="/panomonasabe.jpeg" alt="Rounded avatar">
+        </div>
+        <div class="w-full space-y-3">
+          <div class="flex flex-col justify-start">
+            <label class="font-semibold text-lg">name</label>
+            <div class="flex gap-1 text-xs">
+              <label>@email</label>
+              <span>·</span>
+              <label>date</label>
+            </div>
+          </div>
+          <div class="text-base leading-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+            repellendus aliquam porro cum
+            voluptates, exercitationem dolore tempore amet explicabo? Recusandae atque possimus eaque accusamus sapiente
+            exercitationem voluptate sint dignissimos fugit?</div>
+          <div>
+            <img class="h-auto max-w-lg rounded" src="/panomonasabe.jpeg" alt="image description">
+          </div>
+        </div>
+      </article>
     </main>
   </div>
 </template>
